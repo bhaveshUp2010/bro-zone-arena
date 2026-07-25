415e3c4a84a5adf27954ac2efd9daa0aa97dd635
Bhavesh <bhaav.minor@gmail.com>
2026-07-25 16:07:52 +0530

    Fix filename casing for Render

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/apierror.js";
 import { asynchandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { user } from "../models/user.models.js";
